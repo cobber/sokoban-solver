683b9c5cc03e87907855caca5c6cd60b06f00d07
Stephen Riehm <steve@opensauce.de>
2008-10-19 13:43:34 +0200

    automatic build commit

---
+++ git-prebuild.pl
@@ -0,0 +1,37 @@
+#!/usr/local/bin/perl
+# Xcode auto-versioning script for Subversion by Axel Andersson
+# Updated for git by Marcus S. Zarra and Matt Long
+# Even further revised for automatic commits and nice version numbers by Stephen Riehm
+
+use strict;
+use warnings;
+use POSIX   qw( strftime );
+
+$ENV{'PATH'} =~ s#^#/usr/local/bin:#;
+
+my $timestamp = strftime( "%Y-%m-%d %H:%M:%S %z", localtime() );
+my $git_dir   = qx{ git rev-parse --git-dir };
+chomp( $git_dir );
+exit unless $git_dir;
+$git_dir .= '/..';
+
+# no new version will be created if nothing has changed
+# edit .git/info/excludes to keep junk out
+system( qw{ git add . } );
+
+# commit the current state of all files being used for building
+system( "git", "commit", "-a", "-m", "automatic build commit" );
+
+# git-describe returns the latest tag, the number of patches since that tag, and a commit-ID
+my $git_description = qx{ git describe --long };
+my ( $release, $patch, $commit ) = ( $git_description =~ /([^-]+)-(\d+)-g(\w+)/ );
+$release .= ".${patch}" if $patch;
+$release = "PRE-RELEASE" unless $release;
+
+if( open( VERSION, ">", "$git_dir/version.txt" ) )
+    {
+    print VERSION $release;
+    close( VERSION );
+    }
+
+system( "git", "log", ">", "$git_dir/CHANGELOG" );
